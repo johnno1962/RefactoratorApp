e711be25d2e7039c30ddcaf7bf9043ec2268ab5e
John Holdsworth <github@johnholdsworth.com>
2016-12-08 11:40:35 +0000

    Profiling

--- Refactorator/AppDelegate.swift
+++ Refactorator/AppDelegate.swift
@@ -381,13 +381,18 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate,
         let resp = project?.maps[path] ?? sourceKit.syntaxMap(filePath: path)
         project?.maps[path] = resp
 
+        var extensions = [String:String]()
+
         let dict = sourcekitd_response_get_value( resp )
         let map = sourcekitd_variant_dictionary_get_value( dict, sourceKit.syntaxID )
         sourcekitd_variant_array_apply( map ) { (_,dict) in
             let kind = dict.getUUIDString( key: sourceKit.kindID )
             let offset = dict.getInt( key: sourceKit.offsetID )
             let length = dict.getInt( key: sourceKit.lengthID )
-            let kindSuffix = kind .url.pathExtension
+            let kindSuffix = extensions[kind] ?? kind.url.pathExtension
+            if extensions[kind] == nil {
+                extensions[kind] = kindSuffix
+            }
 
             html += skipTo( offset: offset ).text
 
@@ -887,10 +892,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate,
 }
 
 func htmlEscape( _ str: String ) -> String {
-    return str.replacingOccurrences( of: "&", with: "&amp;" ).replacingOccurrences( of:"<", with: "&lt;" )
+    return str.contains("&") || str.contains("&") ?
+        str.replacingOccurrences(of: "&", with: "&amp;")
+            .replacingOccurrences(of: "<", with: "&lt;") : str
 }
-
-//    ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"]
 
 private let myIndex = {
     () -> Int in
